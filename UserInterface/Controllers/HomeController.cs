b114da2410e012d094ee6b2303285acbdc6ef92d
FoxyChmoxy <almasgaara@mail.ru>
2020-01-19 10:55:31 +0600

    Added MailSending and Help Page

--- UserInterface/Controllers/HomeController.cs
+++ UserInterface/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UserInterface.ViewModels;
@@ -29,6 +33,35 @@ namespace UserInterface.Controllers
         public IActionResult Contacts()
         {
             return View();
+        }
+
+        public IActionResult Help()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Subscribe(string email)
+        {
+            try
+            {
+                MailAddress from = new MailAddress("kz.ebooksharing@gmail.com", "Almas Zaurbekov");
+                MailAddress to = new MailAddress(email);
+                MailMessage m = new MailMessage(from, to);
+                m.Subject = "EBS - Weekly news";
+                m.Body = "<h2>Спасибо большое за подписку!</h2>";
+                m.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.Credentials = new NetworkCredential("kz.ebooksharing@gmail.com", "solune071");
+                smtp.EnableSsl = true;
+                await smtp.SendMailAsync(m);
+                return Redirect("Index?subscribed=yes");
+            }
+            catch (Exception e)
+            {
+                return Redirect("Index?subscribed=no");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
