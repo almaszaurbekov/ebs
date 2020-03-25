758e1d913ed52cc9b7d669c60ce0915fb53881a9
FoxyChmoxy <almasgaara@mail.ru>
2020-03-25 17:38:58 +0600

    Registration bug fixed

--- BusinessLogic/Services/BusinessService/UserBusinessService.cs
+++ BusinessLogic/Services/BusinessService/UserBusinessService.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Caching.Memory;
 using Resources;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
