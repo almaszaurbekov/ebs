ff18e60410c9cdfefba07e1f02b10c8425415da7
FoxyChmoxy <almasgaara@mail.ru>
2020-04-03 16:33:16 +0600

    Task-2: Stored Procedure Migration

---
+++ DataAccess/Migrations/20200403102936_GetBooksByCondition.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class GetBooksByCondition : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			string procedure = @"
+							CREATE PROCEDURE GetBooksByCondition
+								@inGoodCondition bit
+							AS
+							BEGIN
+								SELECT 
+								b.Author,
+								b.Title, 
+
+									(	SELECT TOP 1 c.Text 
+										FROM Comments as c 
+										WHERE b.Id = c.BookId 
+										ORDER BY CreatedDate DESC) as LastCommentText, 
+
+									(	SELECT TOP 1 c.CreatedDate 
+										FROM Comments as c 
+										WHERE b.Id = c.BookId
+										ORDER BY CreatedDate DESC) as LastCommentTime 
+
+								FROM Books AS b
+								WHERE b.InGoodCondition = @inGoodCondition
+								ORDER BY Author;
+							END
+							";
+			migrationBuilder.Sql(procedure);
+		}
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			string procedure = @"DROP PROCEDURE GetBooksByCondition";
+			migrationBuilder.Sql(procedure);
+		}
+    }
+}
