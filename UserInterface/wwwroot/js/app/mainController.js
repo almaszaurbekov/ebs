2056a5a53bdaf0077afe107148da9101bfc847e8
Almas Zaurbekov <foxxychmoxy@gmail.com>
2020-04-16 01:17:40 +0600

    Task-4: Added Book List into User/Index

--- UserInterface/wwwroot/js/app/mainController.js
+++ UserInterface/wwwroot/js/app/mainController.js
@@ -1,6 +1,55 @@
 ﻿class MainController {
     constructor(options = {}) {
         this.options = options;
-        this.contentType = 'application/json; charset=utf-8';
+        this._init();
+    }
+
+    _init() {
+        var url = "/api/ebs/books/index";
+        var data = { "minCount": 5 };
+        var ajax = this.__ajaxQuery("GET", url, data);
+
+        for (let obj of ajax.responseJSON) {
+            var tr = $("<div class='col-md-12 bg-dark text-light py-2 px-2 mb-3'></div>");
+            const { id, title, author, userId } = obj;
+
+            tr.append(`<p>Title: ${title}</p>`);
+            tr.append(`<p>Author: ${author}</p>`);
+
+            this.options.target.append(tr);
+        }
+    }
+
+    __ajaxQuery(method, url, data) {
+        return $.ajax({
+            method: method,
+            url: url,
+            data: data,
+            contentType: "application/json; charset=utf-8",
+            type: "JSON",
+            async: false,
+            beforeSend: function () {
+                $(this.target).empty();
+                $('.wait-pre-con').show();
+            },
+            success: function (result) {
+                $('.wait-pre-con').hide();
+                return result;
+            },
+            error: function (xhr) {
+                $('.wait-pre-con').hide();
+                toastr.error("Failed to process request", "Error");
+                console.log(xhr);
+            }
+        });
     }
 }
+
+$(document).ready(function () {
+
+    var options = {
+        target: $("#book-list-target")
+    };
+
+    var controller = new MainController(options);
+});
