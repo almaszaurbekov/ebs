ced58f2d2d59cdeaee406f2435d05512c12081d3
Almas Zaurbekov <foxxychmoxy@gmail.com>
2020-05-05 04:54:04 +0600

    Task-8: BookOperationsLog entity is added

---
+++ DataAccess/Entities/Base/LogEntity.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Entities.Base
+{
+    public class LogEntity<T>
+    {
+        [Key]
+        public T Id { get; set; }
+
+        [ScaffoldColumn(false)]
+        public EbsLoggerLevel Level { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string LevelText { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string Message { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
