7c59390a50548a6caf13abb24175523ab367c158
Almas Zaurbekov <foxxychmoxy@gmail.com>
2020-05-05 04:54:04 +0600

    Task-8: BookOperationsLog entity is added

---
+++ DataAccess/Entities/BookOperationsLog.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Entities
+{
+    public class BookOperationsLog : LogEntity<int>
+    {
+        public int UserId { get; set; }
+        public string UserEmail { get; set; }
+        public int BookId { get; set; }
+    }
+}
