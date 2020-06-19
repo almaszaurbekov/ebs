f7a0f81f34397ed8262e572fc16192e8b3cc74cf
Almas Zaurbekov <foxxychmoxy@gmail.com>
2020-06-19 19:17:19 +0600

    Cherrypicked from master: ServiceProviderExtensions

---
+++ UserInterface/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogic.Services;
+using BusinessLogic.Services.BusinessService;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UserInterface.Extensions
+{
+
+    public static class ServiceProviderExtensions
+    {
+        public static void AddEbsServices(this IServiceCollection services)
+        {
+            services.AddTransient<IUserBusinessService, UserBusinessService>();
+            services.AddTransient<IBookBusinessService, BookBusinessService>();
+            services.AddTransient<IMessageBusinessService, MessageBusinessService>();
+            services.AddTransient<IAdminBusinessService, AdminBusinessService>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IBookService, BookService>();
+            services.AddTransient<IRoleService, RoleService>();
+            services.AddTransient<IMessageService, MessageService>();
+            services.AddTransient<IDialogControlService, DialogControlService>();
+            services.AddTransient<ICommentService, CommentService>();
+            services.AddTransient<IBcBookService, BcBookService>();
+            services.AddTransient<IBookTransactionService, BookTransactionService>();
+        }
+    }
+}
